2648627611acf833b74a3459e9c62d71f88f18ae
Olga <koa074@gmail.com>
2018-09-04 08:37:46 -0400

    handle visdom

--- train_model.py
+++ train_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
 from tqdm import tqdm
@@ -10,6 +9,16 @@ from utils import variable, idx2text, save_weights
 
 
 def check_random_example(dataset, model, idx2w):
+    """
+    Check a random test sentence and its autoencoder reconstruction
+    Args:
+        dataset: AutoencoderDataset
+        model: Autoencoder model
+        idx2w (dict): A reversed dictionary matching ids to tokens
+
+    Returns (tuple): input training sentence and its reconstruction
+
+    """
     random_idx = np.random.randint(len(dataset))
     input = dataset[random_idx]
     input = variable(input)
@@ -21,24 +30,29 @@ def check_random_example(dataset, model, idx2w):
     return input_text, predicted_text
 
 
-def train(model, filename, criterion, optimizer, dataloaders, n_epochs, idx2w, max_grad_norm=5):
+def train(model, filename, criterion, optimizer, dataloaders, n_epochs, idx2w, max_grad_norm=5, port=None):
     """
+    The model training function. Saves a model to a specified location if the loss on the dev set reaches a new minimum
+    Args:
+        model:
+        filename (str):  A path for saving a trained model
+        criterion: Used loss function
+        optimizer: torch optimizer
+        dataloaders (dict): a dictionary of a train and test DataLoader instances
+        n_epochs: Number of epochs
+        idx2w (dict): A reversed dictionary matching ids to tokens
+        max_grad_norm (optional): Maximum magnitude of gradients (for clipping)
+        port: Used for connecting to Visdom server and visualizing the learning curve
 
-    :param model:
-    :param filename:
-    :param criterion:
-    :param optimizer:
-    :param dataloaders:
-    :param n_epochs:
-    :param idx2w:
-    :param max_grad_norm:
-    :return:
     """
 
     parameters = [parameter for parameter in list(model.parameters()) if parameter.requires_grad]
-    viz = Visdom(port=8098)
-    viz.line(X=np.array([0]), Y=np.expand_dims(np.array([0, 0]), axis=0), win='Loss',
-             opts={'title': 'Loss', 'legend': ['Train', 'Dev']}, )
+
+    # Prepare for visualization if Visdom server is running
+    if port:
+        viz = Visdom(port=port)
+        viz.line(X=np.array([0]), Y=np.expand_dims(np.array([0, 0]), axis=0), win='Loss',
+                 opts={'title': 'Loss', 'legend': ['Train', 'Dev']}, )
 
     best = None
 
@@ -60,6 +74,7 @@ def train(model, filename, criterion, optimizer, dataloaders, n_epochs, idx2w, m
                 mask = torch.ne(inputs, 0).long()
                 predictions_masked = predictions*mask
                 match = predictions_masked.eq(inputs)
+
                 correct += torch.sum(torch.eq(torch.sum(match, dim=1), inputs.shape[1])).item()
                 total += inputs.shape[0]
 
@@ -97,5 +112,6 @@ def train(model, filename, criterion, optimizer, dataloaders, n_epochs, idx2w, m
                     print("{: <20} {}".format("Input:", input_text))
                     print("{: <20} {}".format("Reconstructed:", predicted_text))
 
-        viz.line(X=np.array([epoch]), Y=np.expand_dims(np.array([loss_train, loss_dev]), axis=0),
-                 win='Loss', update='append')
+        if port:
+            viz.line(X=np.array([epoch]), Y=np.expand_dims(np.array([loss_train, loss_dev]), axis=0),
+                     win='Loss', update='append')
